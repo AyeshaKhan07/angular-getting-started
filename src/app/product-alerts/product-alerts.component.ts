294f582f12160b44d2b3d75370ec0099530ceb87
AyeshaKhan07 <ayeshanadeem9085@gmail.com>
2022-04-22 15:30:52 +0500

    5 files modified, 4 added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -9,6 +9,7 @@ import {Product} from "../products";
 export class ProductAlertsComponent implements OnInit {
   @Input() product!: Product;
   @Output() notify = new EventEmitter();
+  @Output() share = new EventEmitter();
   
   constructor() { }
 
