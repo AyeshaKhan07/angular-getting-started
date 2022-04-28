2f401244d8323d0dcbcae6186de1962eb0bf4883
AyeshaKhan07 <ayeshanadeem9085@gmail.com>
2022-04-28 17:39:33 +0500

    4 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,20 +1,42 @@
 import { Injectable } from '@angular/core';
-import {Product} from "./products"
+import { Product } from './products';
+
+export interface Item {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  quantity: number;
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
-  items: Product[] = [];
+  items: Item[] = [];
 
   addToCart(product: Product) {
-    this.items.push(product)
+    const index = this.items.findIndex((item) => item.id == product.id);
+    if (index >= 0) {
+      ++this.items[index].quantity;
+    } else {
+      let item = {
+        ...product,
+        quantity: 1,
+      };
+      this.items.push(item);
+    }
+  }
+
+  deleteItem(product: Item) {
+    this.items.filter((item) => item.id !== product.id);
   }
   getItems() {
-    return this.items
+    return this.items;
   }
   clearCart() {
     this.items = [];
     return this.items;
   }
-  constructor() { }
+  constructor() {}
 }
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CartService } from '../cart.service';
+import { CartService, Item } from '../cart.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -8,11 +8,14 @@ import { CartService } from '../cart.service';
 export class CartComponent implements OnInit {
   constructor(private cartService: CartService) {}
   items = this.cartService.items;
-  count = 0
+  count = 0;
   getCount() {
-    if(this.count == this.items.length)
-      this.count = 0
-    return ++this.count
+    if (this.count == this.items.length) this.count = 0;
+    return ++this.count;
+  }
+  deleteItem(item: Item) {
+    this.cartService.deleteItem(item);
+    window.alert('Item deleted');
   }
   ngOnInit(): void {}
 }
